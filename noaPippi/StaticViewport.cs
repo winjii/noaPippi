8bd597f51a631fdf4a2bc4a4c12ef6b5f851afe8
うぃんじー <wing3196@gmail.com>
2017-04-11 02:01:17 +0900

    StaticViewportを追加

---
+++ noaPippi/StaticViewport.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace noaPippi
+{
+    class StaticViewport : IVirtualViewport
+    {
+        public override double GetRateOfHeight()
+        {
+            throw new NotImplementedException();
+        }
+        public override double GetRateOfWidth()
+        {
+            throw new NotImplementedException();
+        }
+        public override double GetRateOfX()
+        {
+            throw new NotImplementedException();
+        }
+        public override double GetRateOfY()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
