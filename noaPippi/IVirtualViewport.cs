40c94946e771724d0a9aba6afe9826f5d3acf3c2
winjii <wing3196@gmail.com>
2017-04-10 12:00:27 +0900

    やっぱり気になるから抽象クラスにしようかなーって

--- noaPippi/IVirtualViewport.cs
+++ noaPippi/IVirtualViewport.cs
@@ -10,13 +10,21 @@ namespace noaPippi
 {
     //TODO: 固定された表示領域と動的な表示領域のクラスを派生させたい
     //TODO: クラスの多態性の実装で、継承を使うのがフィールドで型を持つのより何が優れているのか調べる
-    interface IVirtualViewport
+    abstract class IVirtualViewport
     {
-        IVirtualViewport parent;
-        double GetRateOfX();
-        double GetRateOfY();
-        double GetRateOfWidth();
-        double GetRateOfHeight();
-        Viewport GetViewport();
+        protected IVirtualViewport parent;
+        public double GetRateOfX();
+        public double GetRateOfY();
+        public double GetRateOfWidth();
+        public double GetRateOfHeight();
+        public Viewport GetAbsoluteViewport()
+        {
+            return new Viewport(
+                parent.X + parent.Width*GetRateOfX(),
+                parent.Y + parent.Height*GetRateOfY(),
+                parent.Width*GetRateOfWidth(),
+                parent.Height*GetRateOfHeight
+            );
+        }
     }
 }
