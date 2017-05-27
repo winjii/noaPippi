10ed51323a9596e27f9ab7f66d0d9c2a428ed5f7
うぃんじー <wing3196@gmail.com>
2017-05-27 23:07:06 +0900

    RelativeViewportの実装(未テスト)

--- noaPippi/RelativeViewport.cs
+++ noaPippi/RelativeViewport.cs
@@ -8,47 +8,120 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace noaPippi
 {
-    //TODO: 固定された表示領域と動的な表示領域のクラスを派生させたい
-    class RelativeViewport
+    //TODO: 毎回再帰で描画領域を求めているのをメモ化で高速化
+    abstract class RelativeViewport
     {
-        protected RelativeViewport parent;
-        private double rateOfX;
-        private double rateOfY;
-        private double rateOfWidth;
-        private double rateOfHeight;
+        private List<Children> children;
+        public bool IsHorizontal { get; set; }
 
-        protected RelativeViewport(RelativeViewport parent)
+        protected RelativeViewport(bool isHorizontal)
         {
-            if (parent == null) this.parent = this;
-            else this.parent = parent;
+            children = new List<Children>();
+            IsHorizontal = isHorizontal;
         }
-        public double GetX() { return parent.GetWidth()*rateOfX; }
-        public double GetY() { return parent.GetHeight()*rateOfY; }
-        public double GetWidth() { return parent.GetWidth()*rateOfWidth; }
-        public double GetHeight() { return parent.GetHeight()*rateOfHeight; }
-        public double GetRateOfX() { return rateOfX; }
-        public double GetRateOfY() { return rateOfY; }
-        public double GetRateOfWidth() { return rateOfWidth; }
-        public double GetRateOfHeight() { return rateOfHeight; }
-        public bool IsRoot() { return parent == this; }
-        public double GetAbsoluteX()
+        
+        public void AddChildren(double rateOfLength, bool isHorizontal, bool isStatic)
         {
-            if (IsRoot()) return GetX();
-            return parent.GetX() + GetX();
+            double rateOfPos = children.Last().GetAbsolutePos() + children.Last().GetAbsoluteLength();
+            Children tmp = new Children(this, rateOfPos, rateOfLength, isHorizontal, isStatic);
+            children.Add(tmp);
         }
-        public double GetAbsoluteY()
+        virtual public void Recalculate()
         {
-            if (IsRoot()) return GetY();
-            return parent.GetY() + GetY();
+            foreach (Children c in children)
+            {
+                c.Recalculate();
+            }
         }
+        abstract public double GetAbsoluteX();
+        abstract public double GetAbsoluteY();
+        abstract public double GetAbsoluteWidth();
+        abstract public double GetAbsoluteHeight();
         public Viewport GetIntViewport()
         {
             return new Viewport(
                 (int)(GetAbsoluteX() + 0.5),
                 (int)(GetAbsoluteY() + 0.5),
-                (int)(GetWidth() + 0.5),
-                (int)(GetHeight() + 0.5)
+                (int)(GetAbsoluteWidth() + 0.5),
+                (int)(GetAbsoluteHeight() + 0.5)
             );
+        }
+
+
+        class Children : RelativeViewport
+        {
+            protected RelativeViewport parent;
+            public bool IsStatic { get; set; }
+            public double RateOfPos { get; }
+            public double RateOfLength { get; }
+
+            public Children(RelativeViewport parent, double rateOfPos, double rateOfLength, bool isHorizontal, bool isStatic) : base(isHorizontal)
+            {
+                this.parent = parent??throw new ArgumentNullException();
+                RateOfPos = rateOfPos;
+                RateOfLength = rateOfLength;
+                IsStatic = isStatic;
+            }
+            
+            public double GetAbsolutePos()
+            {
+                if (parent.IsHorizontal) return GetAbsoluteX();
+                return GetAbsoluteY();
+            }
+            public double GetAbsoluteLength()
+            {
+                if (parent.IsHorizontal) return GetAbsoluteWidth();
+                return GetAbsoluteHeight();
+            }
+            public override double GetAbsoluteX()
+            {
+                if (!parent.IsHorizontal) return 0;
+                return parent.GetAbsoluteX() + parent.GetAbsoluteWidth()*RateOfPos;
+            }
+            public override double GetAbsoluteY()
+            {
+                if (parent.IsHorizontal) return 0;
+                return parent.GetAbsoluteY() + parent.GetAbsoluteHeight()*RateOfPos;
+            }
+            public override double GetAbsoluteWidth()
+            {
+                if (!parent.IsHorizontal) return parent.GetAbsoluteWidth();
+                return parent.GetAbsoluteWidth()*RateOfLength;
+            }
+            public override double GetAbsoluteHeight()
+            {
+                if (parent.IsHorizontal) return parent.GetAbsoluteHeight();
+                return parent.GetAbsoluteHeight()*RateOfLength;
+            }
+        }
+
+        class Root : RelativeViewport
+        {
+            private double absoluteWidth;
+            private double absoluteHeight;
+
+            public Root(double absoluteWidth, double absoluteHeight, bool isHorizontal) : base(isHorizontal)
+            {
+                this.absoluteWidth = absoluteWidth;
+                this.absoluteHeight = absoluteHeight;
+            }
+
+            public override double GetAbsoluteX()
+            {
+                return 0;
+            }
+            public override double GetAbsoluteY()
+            {
+                return 0;
+            }
+            public override double GetAbsoluteWidth()
+            {
+                return absoluteWidth;
+            }
+            public override double GetAbsoluteHeight()
+            {
+                return absoluteHeight;
+            }
         }
     }
 }
