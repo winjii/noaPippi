ae7543a48c2338d07bfc194657a0cb0a331b935c
うぃんじー <wing3196@gmail.com>
2017-07-13 01:20:42 +0900

    AfterTouch.csを追加

---
+++ noaPippi/Midi/AfterTouch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noaPippi.Midi
+{
+    class AfterTouch
+    {
+        public byte Channel { get; }
+        public byte Key { get; }
+        public byte Level { get; }
+        public AfterTouch(byte key, byte level)
+        {
+            Key = key;
+            Level = level;
+        }
+    }
+}
