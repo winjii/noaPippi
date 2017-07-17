a38f172f20e898f15b6f4280515abad09bd01a25
うぃんじー <wing3196@gmail.com>
2017-07-18 02:39:49 +0900

    コミット漏れ(保存忘れ)

--- noaPippi/StaffNotation.cs
+++ noaPippi/StaffNotation.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System.Xml;
 
 namespace noaPippi
 {
@@ -13,10 +14,12 @@ namespace noaPippi
         double lowerLineY = 0.75;
         double lineDiff = 0.5/4.0;
         public Clef MyClef { get; }
+        List<List<object>> measures;
         private double[] noteY;
 
         public StaffNotation(Clef.ClefType clefType, Game game, RelativeViewport viewport) : base(game, viewport)
         {
+            measures = new List<List<object>>();
             MyClef = new Clef(clefType);
             noteY = new double[128];
             int b = MyClef.NoteNumberOfLowestLine;
@@ -31,10 +34,28 @@ namespace noaPippi
                 noteY[i] = noteY[i + 1] + d[i%12]*lineDiff/2d;
             }
         }
-
-        public double NoteToY(int key)
+        public void Load(XmlDocument doc)
         {
-            return noteY[key];
+            this.measures = new List<List<object>>();
+            XmlNodeList measures = doc.SelectNodes("part/measure");
+            XmlNode attributes = measures[0]["attributes"];
+            int divisions = int.Parse(attributes["divisions"].InnerText);
+            //TODO: 調に対応
+            //XmlNode noteNumber = attributes.SelectSingleNode("noteNumber");
+            //TODO: 拍子に対応
+            //XmlNode time = attributes.SelectSingleNode("time");
+            foreach (XmlNode measure in measures)
+            {
+                foreach (XmlNode note in measure)
+                {
+                    XmlNode pitch = note["pitch"];
+                    pitch[]
+                }
+            }
+        }
+        public double NoteToY(int noteNumber)
+        {
+            return noteY[noteNumber];
         }
         private void test(SpriteBatch spriteBatch)
         {
